a97e822a6acd369bd2c3a5f242c5e5a58cda40f5
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-11-14 13:16:16 +0100

    Update receive-crypto-dialog.tsx

--- client/src/components/receive-crypto-dialog.tsx
+++ client/src/components/receive-crypto-dialog.tsx
@@ -21,7 +21,7 @@ import { getDepositAddress } from "@/lib/wallet-api";
 import { useAuth } from "@/lib/auth-context";
 import { QRCodeSVG } from "qrcode.react";
 import { cryptoIconUrls } from "@/lib/crypto-icons";
-import { useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/hooks/use-toast";
 import { Input } from "@/components/ui/input";
 
 
