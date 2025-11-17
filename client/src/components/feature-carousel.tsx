89ce1ed5ca88c7d84c971d5cd052eaf368200e02
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-11-17 14:59:36 -0800

    Update feature-carousel.tsx

--- client/src/components/feature-carousel.tsx
+++ client/src/components/feature-carousel.tsx
@@ -63,7 +63,7 @@ export function FeatureCarousel() {
   };
 
   return (
-    <section className="relative h-[500px] sm:h-[550px] md:h-[600px] w-full overflow-hidden px-4 mb-8">
+    <section className="relative h-[550px] sm:h-[600px] md:h-[650px] lg:h-[680px] w-full overflow-hidden px-4 mb-4">
       <div
         className="relative h-full w-full max-w-7xl mx-auto"
         style={{ perspective: "2000px", perspectiveOrigin: "center center" }}
