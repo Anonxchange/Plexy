7b80bc9c7860ff6fda504c3faaba30a005e2aae8
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-11-24 22:35:02 -0800

    Update pexly-pay.tsx

--- client/src/pages/pexly-pay.tsx
+++ client/src/pages/pexly-pay.tsx
@@ -82,7 +82,7 @@ export default function PexlyPay() {
           })
           .select()
           .single();
-        
+
         if (newProfile) {
           setProfileData(newProfile);
         }
