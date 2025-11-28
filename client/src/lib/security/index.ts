b0b9657a83688124a291a9a46dc318cf5d035dbf
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-11-28 03:20:20 -0800

    Update index.ts

--- client/src/lib/security/index.ts
+++ client/src/lib/security/index.ts
@@ -3,5 +3,6 @@ export * from './risk-engine';
 export * from './wallet-manager';
 export * from './withdrawal-whitelist';
 export * from './aml-screening';
+export * from './aml-workflow';
 export * from './ip-whitelist';
 export * from './device-fingerprint';
