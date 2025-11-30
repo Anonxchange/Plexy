96df238d6c4f393dcf28a524d4faac25efa842e5
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-11-30 04:26:19 -0800

    Update referral.tsx

--- client/src/pages/referral.tsx
+++ client/src/pages/referral.tsx
@@ -68,7 +68,7 @@ export default function ReferralPage() {
             <div className="mb-8 flex justify-center">
               <div className="relative w-64 h-64">
                 <img 
-                  src="/assets/IMG_1961.jpeg" 
+                  src="/assets/IMG_2668.png" 
                   alt="Referral illustration" 
                   className="w-full h-full object-contain"
                 />
@@ -238,7 +238,7 @@ export default function ReferralPage() {
             <div className="mb-6 flex justify-center">
               <div className="relative w-48 h-48">
                 <img 
-                  src="/assets/IMG_1961.jpeg" 
+                  src="/assets/IMG_2668.png" 
                   alt="Refer & Win" 
                   className="w-full h-full object-contain"
                 />
