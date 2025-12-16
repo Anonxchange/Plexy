893967a8d31ff724f7c555fd1b6fa6569035d09c
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-12-15 19:51:10 -0800

    Update crypto-icons.ts

--- client/src/lib/crypto-icons.ts
+++ client/src/lib/crypto-icons.ts
@@ -7,6 +7,10 @@ export const cryptoIconUrls: Record<string, string> = {
   USDC: "https://assets.coingecko.com/coins/images/6319/small/usdc.png",
   USDT: "https://assets.coingecko.com/coins/images/325/small/Tether.png",
   LTC: "https://assets.coingecko.com/coins/images/2/small/litecoin.png",
+  TON: "https://assets.coingecko.com/coins/images/17980/small/ton_symbol.png",
+  MNT: "https://assets.coingecko.com/coins/images/30980/small/token-logo.png",
+  USDE: "https://assets.coingecko.com/coins/images/33613/small/usde.png",
+  PYUSD: "https://assets.coingecko.com/coins/images/31212/small/PYUSD_Logo_%282%29.png",
 };
 
 export const getCryptoIconUrl = (symbol: string): string => {
