79fb9dc098c2356cd9b9611c2f086797b224b793
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2026-03-17 17:18:47 -0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { createHead, UnheadProvider } from "@unhead/react/client";
 import App from "./App";
 import "./index.css";
 
+const head = createHead();
+
 const rootElement = document.getElementById("root");
 if (rootElement) {
-  createRoot(rootElement).render(<App />);
+  createRoot(rootElement).render(
+    <UnheadProvider head={head}>
+      <App />
+    </UnheadProvider>
+  );
 }
