0766c3fa6143a12d2cd6db81580ec6995f34e1f6
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2026-06-15 21:07:40 +0100

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,4 @@
 import "@fontsource-variable/plus-jakarta-sans";
-import "@fontsource-variable/inter";
 import "@fontsource-variable/jetbrains-mono";
 import "@/lib/i18n";
 import { startAutoTranslate } from "@/lib/auto-translate";
